5b423066cc95340e2287414105bf4b228db5c3dc
Jack Christiansen <christj6@tcnj.edu>
2014-04-20 13:21:18 -0400

    One way of grabbing user input in Haskell
    I might tinker around with it to grab other data types

---
+++ Input.hs
@@ -0,0 +1,18 @@
+
+-- How I grab integers from the user in Haskell.
+module Input where
+
+import Data.Maybe (listToMaybe)
+
+-- function for verifying user input: http://stackoverflow.com/questions/2931557/haskell-check-for-user-input-errors
+maybeRead :: Read a => String -> Maybe a
+maybeRead = fmap fst . listToMaybe . filter (null . snd) . reads
+
+-- used for grabbing any sort of user input. Uses recursion to reprompt in event of user input errors.
+grab :: IO(Int)
+grab = do
+	putStr "Enter an integer: "
+	x <- fmap maybeRead getLine :: IO (Maybe Int)
+	case x of
+  	  Just x -> return x
+  	  Nothing -> grab
